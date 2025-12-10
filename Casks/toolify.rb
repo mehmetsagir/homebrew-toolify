19a65fc0ac9e52d9a6dc62c695dc8e20f918c385
Mehmet Sağır <memetsagirint@gmail.com>
2025-12-10 12:57:53 +0300

    chore: update formula version to 0.0.7

--- Casks/toolify.rb
+++ Casks/toolify.rb
@@ -1,5 +1,5 @@
 cask "toolify" do
-  version "0.0.6"
+  version "0.0.7"
   sha256 :no_check
 
   url "https://github.com/mehmetsagir/toolify/releases/download/v#{version}/toolify-#{version}-arm64.dmg"
